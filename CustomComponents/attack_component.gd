85ed7f9248e812b0760d42aae1f42bf409211421
lunaro <pavelike@gmail.com>
2024-03-09 12:24:30 +0300

    Initial

---
+++ CustomComponents/attack_component.gd
@@ -0,0 +1,41 @@
+class_name AttackComponent extends Node
+
+@export var delay : float = 0
+@export var hit_window : float
+@export var speed_modifyer : float = 1
+@export var hitbox : HitBoxComponent
+
+
+signal attack_finished
+
+
+func attack():
+	#print("Hit initialized")
+	wait(delay * speed_modifyer)
+	var hitbox_collisions_array := hitbox.get_children().filter(func(shape):
+		return shape.get_class() == "CollisionShape2D")
+	swich_hitbox_state(hitbox_collisions_array, false)
+	await wait(hit_window * speed_modifyer)
+	swich_hitbox_state(hitbox_collisions_array, true)
+	#print("Hit finished")
+	attack_finished.emit()
+
+func wait(time) -> void:
+	#print("Timer started for ", time)
+	await get_tree().create_timer(time).timeout
+	#print("Timer ended.")
+	
+
+
+
+func swich_hitbox_state(array : Array, state : bool):
+	for item in array:
+		item.disabled = state
+
+func _ready():
+	if speed_modifyer == 0:
+		speed_modifyer = 1
+	else:
+		speed_modifyer = 1 / speed_modifyer
+	pass
+
---
+++ CustomComponents/move_to_direction.gd
@@ -0,0 +1,11 @@
+class_name Movement
+
+
+static func move_to_direction(direction: Vector2, speed: float):
+	#direction = pathfinder.target_path_direction
+	#velocity = direction * speed
+	#move_and_slide()
+	
+	pass
+	
+	
