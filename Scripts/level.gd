ff08704bc7ca43a57929e3f4ab02577c34f903f6
lunaro <pavelike@gmail.com>
2024-03-11 00:12:55 +0300

    Закончил выбор кнопки, работа над выбором врага через кнопку

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -10,8 +10,14 @@ var slime = preload("res://Scenes/slime.tscn")
 @onready var creatures : Array[String] = ["None", "Slime", "Goblin"]
 @onready var chosen_creature = creatures[0]
 
+@onready var goblin_unlocked : bool = true
+
 @onready var slime_button = %SlimeButton as TextureButton
 @onready var goblin_button = %GoblinButton as TextureButton
+
+@onready var slime_panel = %SlimePanel as Panel
+@onready var goblin_panel = %GoblinPanel as Panel
+
 @onready var active_refresh: bool = false 
 
 func _ready():
@@ -31,16 +37,24 @@ func refresh_gold_lable():
 func _process(_delta):
 	if Input.is_action_just_pressed("LMBclick"):
 		if current_gold >= 0:
-			var enemy_instance = slime.instantiate() as Slime
-			if player != null:
-				enemy_instance.player = player
-			enemy_instance.position = get_global_mouse_position()
-			enemy_instance.is_static = false
-			current_gold -=5
-			refresh_gold_lable()
-			add_child(enemy_instance)
+			if chosen_creature == creatures[0]:
+				return
+			elif chosen_creature == creatures[1]:
+				spawn_enemy(slime, 5)
+			
 		else:
 			print("not enough gold!")
+	pass
+
+func spawn_enemy(type, cost):
+	var enemy_instance = type.instantiate()
+	if player != null:
+		enemy_instance.player = player
+	enemy_instance.position = get_global_mouse_position()
+	enemy_instance.is_static = false
+	current_gold -= cost
+	refresh_gold_lable()
+	add_child(enemy_instance)
 	pass
 
 
@@ -48,26 +62,43 @@ func _on_player_got_hit(value):
 	current_gold += value
 	refresh_gold_lable()
 	
+
+func button_toggle(button : TextureButton,panel : Panel , state: bool):
+	button.button_pressed = state
+	var color : int
+	if state:
+		color = 255
+	else:
+		color = 0
+	panel.modulate.a = color
+	print(panel.self_modulate.a)
+	
+	pass
+	
+	
 func buttons_refresh():
 	active_refresh = true
-	slime_button.button_pressed = false
-	goblin_button.button_pressed = false
+	button_toggle(slime_button, slime_panel, false)
+	button_toggle(goblin_button, goblin_panel, false)
 	if chosen_creature == creatures[0]:
 		pass
 	elif chosen_creature == creatures[1]:
-		slime_button.button_pressed = true
+		button_toggle(slime_button, slime_panel, true)
 	elif chosen_creature == creatures[2]:
-		goblin_button.button_pressed = true
+		button_toggle(goblin_button, goblin_panel, true)
 	else:
 		chosen_creature == creatures[0]
-	print("Final choice:  ",chosen_creature)
+	print("Choice:  ",chosen_creature)
 	active_refresh = false
 
 func _on_goblin_button_toggled(toggled_on):
-	print("Goblin ", chosen_creature)
+	#print("Goblin ", chosen_creature)
+	if !goblin_unlocked:
+		return
 	if toggled_on and !active_refresh:
 		chosen_creature = creatures[2]
-		print("Goblin chosen ", chosen_creature)
+		#print("Goblin chosen ", chosen_creature)
+		buttons_refresh()
 	elif chosen_creature == creatures[2] and !active_refresh:
 		chosen_creature = creatures[0]
 		buttons_refresh()
@@ -75,10 +106,11 @@ func _on_goblin_button_toggled(toggled_on):
 
 
 func _on_slime_button_toggled(toggled_on):
-	print("Slime " , chosen_creature)
+	#print("Slime " , chosen_creature)
 	if toggled_on and !active_refresh:
 		chosen_creature = creatures[1]
-		print("Slime chnosen ", chosen_creature)
+		#print("Slime chnosen ", chosen_creature)
 		buttons_refresh()
 	elif chosen_creature == creatures[1] and !active_refresh:
 		chosen_creature = creatures[0]
+		buttons_refresh()
