5c2514096b10fad5770320c0b9b4c24c4555bb45
lunaro <pavelike@gmail.com>
2024-03-10 20:00:09 +0300

    Босс добавлен

--- Scripts/player_start_scene.gd
+++ Scripts/player_start_scene.gd
@@ -1,13 +1,37 @@
 extends Node2D
 
+@onready var enemy_one = preload("res://Scenes/enemy_1.tscn")
 
 @onready var camera = $Camera2D as Camera2D
 @onready var player = $PlayerCharacter as ControlablePlayer
-@onready var Boss = $Boss
+
+@onready var boss_spawn_pos = $BossSpawn as Marker2D
+
+@onready var boss = $"BF"
+@onready var FMT = $"AudioStreamPlayer"
 
 func _process(delta):
 	if camera.global_position.y - 100 < player.global_position.y:
 		camera.global_position.y += 5
 	if camera.global_position.y + 100 > player.global_position.y:
 		camera.global_position.y -= 5
-		
+
+
+
+func spawn_boss():
+	var boss_instance = enemy_one.instantiate() as EnemyOne
+	if player != null:
+		boss_instance.player = player
+	boss_instance.position = boss_spawn_pos.global_position
+	boss_instance.is_static = false
+	add_child(boss_instance)
+
+
+
+func _on_slime_death():
+	if get_children().filter(func(entity): 
+		return entity.has_signal("on_slime_death")).size() < 2 :
+		boss.play()
+		FMT.stop()
+		spawn_boss()
+	pass 
--- Scripts/slime_attack_1.gd
+++ Scripts/slime_attack_1.gd
@@ -2,15 +2,19 @@ extends Polygon2D
 
 
 @export var main_sprite : AnimatedSprite2D
-
+@export var attack_sprite : AnimatedSprite2D
 
 
 func _ready():
 	pass
 
 func animate(_delay_wait, _hit_wait):
-	color.a = 0.8
-	var fade_interval = _hit_wait/20
-	for i in range(20):
-		await get_tree().create_timer(fade_interval).timeout
-		color.a -= 0.05 
+	main_sprite.visible = false
+	attack_sprite.visible = true
+	attack_sprite.play("", (6/_hit_wait)*3) 
+
+
+func _on_attack_sprite_animation_finished():
+	main_sprite.visible = true
+	attack_sprite.visible = false
+	attack_sprite.stop()
