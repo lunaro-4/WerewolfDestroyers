dfa912aaec4b3d9db43088958c9e4fc412e4335a
lunaro <pavelike@gmail.com>
2024-03-11 13:48:09 +0300

    + спрайты

---
+++ Scripts/player_attack_1_animation.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+@export var animation_polygon : Polygon2D
+
+func animate(_delay_wait, _hit_wait):
+	animation_polygon.animate(_delay_wait, _hit_wait)
+	
+	
--- Scripts/slime_attack_1.gd
+++ Scripts/slime_attack_1.gd
@@ -12,7 +12,7 @@ func _ready():
 func animate(_delay_wait, _hit_wait):
 	main_sprite.visible = false
 	attack_sprite.visible = true
-	attack_sprite.play("", 6/_hit_wait) 
+	attack_sprite.play("", attack_sprite.sprite_frames.get_frame_count("default")/_hit_wait) 
 
 
 func _on_attack_sprite_animation_finished():
