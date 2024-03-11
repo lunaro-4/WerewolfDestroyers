6147f508928d05c71642651f7d6ab0036746b9cf
lunaro <pavelike@gmail.com>
2024-03-11 18:35:57 +0300

    Герой едет

--- Scripts/player_character.gd
+++ Scripts/player_character.gd
@@ -9,10 +9,17 @@ class_name Player extends CharacterBody2D
 
 @onready var not_second_level : bool = true
 
+@onready var pathfinder = $PathfindingLogic as PathfinderLogic
+
+
+@onready var direction = pathfinder.target_path_vector
+
 
 @export var target_location : Node2D
 
 const DIFFICULTY_RAMP := 0.01
+
+const SPEED = 100
 
 signal player_got_hit(value)
 
@@ -21,12 +28,35 @@ signal player_got_hit(value)
 func _ready():
 	attack_component.attack()
 	$RegenerationComponent.regen_start()
+	if target_location:
+		pathfinder.target = target_location
+		pathfinder.pathfinding_init()
+	
 
 	pass 
 
 
 func _on_death():
 	queue_free()
+
+func _physics_process(_delta):
+	#print(is_static)
+	#if is_static == true:
+		#animate_sprite(false)
+	#elif is_static == false:
+		#animate_sprite(true)
+		#direction = pathfinder.target_path_vector
+		#if direction.x< 0:
+			#main_sprite.flip_h = true
+			#attack_sprite.flip_h = true
+		#else:
+			#main_sprite.flip_h = false
+			#attack_sprite.flip_h = false
+		direction = pathfinder.target_path_vector
+		velocity = -direction * SPEED
+		
+		
+		move_and_slide()
 
 
 
