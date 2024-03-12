344fe166fad9351eb790aa5c0c07da7e79af6492
lunaro <pavelike@gmail.com>
2024-03-12 22:33:26 +0300

    Изменения в анимации; добавил новый компонент

--- Scripts/slime_attack_1.gd
+++ Scripts/slime_attack_1.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 @export var main_sprite : AnimatedSprite2D
 @export var attack_sprite : AnimatedSprite2D
+@export var animation_component : AnimationComponent
 
 #signal here_animation_finished
 
@@ -10,16 +11,18 @@ func _ready():
 	pass
 
 func animate(_hit_wait):
-	attack_sprite.sprite_frames.set_animation_speed("default", 1)
-	attack_sprite.set_frame(0)
-	main_sprite.visible = false
-	attack_sprite.visible = true
-	attack_sprite.play("", attack_sprite.sprite_frames.get_frame_count("default")/_hit_wait) 
+	animation_component.animate_attack(_hit_wait)
+	#attack_sprite.sprite_frames.set_animation_speed("default", 1)
+	#attack_sprite.set_frame(0)
+	#main_sprite.visible = false
+	#attack_sprite.visible = true
+	#attack_sprite.play("", attack_sprite.sprite_frames.get_frame_count("default")/_hit_wait) 
 
 
 func _on_attack_sprite_animation_finished():
 	#print("signal!")
 	#here_animation_finished.emit()
-	main_sprite.visible = true
-	attack_sprite.visible = false
-	attack_sprite.stop()
+	#main_sprite.visible = true
+	#attack_sprite.visible = false
+	#attack_sprite.stop()
+	pass
